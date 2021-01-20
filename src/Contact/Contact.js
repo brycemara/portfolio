69b0bb5bbfa9e67d25a30ba0d221e26e6890982a
Bryce Mara Jarrett <brycej98@gmail.com>
2021-01-20 13:06:03 -0600

    Add projects

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -4,7 +4,7 @@ function Contact() {
   return(
     <section >
       <h1 className="contact-title">Contact Me</h1>
-      <div className="contact">
+      <div className="contact-me">
         <a target="blank" href="https://www.linkedin.com/in/bryce-jarrett-496496171/"><img alt="linkedin"  src="https://img.shields.io/badge/-LinkedIn-black.svg?style=for-the-badge&logo=linkedin&colorB=1C5D99"/></a>
         <a target="blank"href="https://github.com/brycemara"><img alt="github" src="https://img.shields.io/badge/-Github-b1361e.svg?style=for-the-badge&logo=github&colorB=black" /></a>
         <a target="blank" href="mailto:brycej98@gmail.com"><img alt="email" src="https://img.shields.io/badge/-Email-f2c236.svg?style=for-the-badge&colorB=0078D4" /></a>
