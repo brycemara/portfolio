5bf906b08fcb1b044c4a5e9c79cac0bb2c3a6f7b
Bryce Mara Jarrett <brycej98@gmail.com>
2021-01-20 12:53:13 -0600

    Contact Me

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -2,11 +2,12 @@ import './Contact.css'
 
 function Contact() {
   return(
-    <section>
-      <div classNam="contact">
-        <a href="https://www.linkedin.com/in/bryce-jarrett-496496171/"><img alt="linkedin"  src="https://img.shields.io/badge/-LinkedIn-black.svg?style=for-the-badge&logo=linkedin&colorB=1C5D99"/></a>
-        <a href="https://www.codewars.com/users/brycemara"><img alt="codewars" src="https://img.shields.io/badge/-Codewars-b1361e.svg?style=for-the-badge&logo=codewars&colorB=b1361e" /></a>
-        <a href="mailto:brycej98@gmail.com"><img alt="email" src="https://img.shields.io/badge/-Email-f2c236.svg?style=for-the-badge&colorB=0078D4" /></a>
+    <section >
+      <h1 className="contact-title">Contact Me</h1>
+      <div className="contact">
+        <a target="blank" href="https://www.linkedin.com/in/bryce-jarrett-496496171/"><img alt="linkedin"  src="https://img.shields.io/badge/-LinkedIn-black.svg?style=for-the-badge&logo=linkedin&colorB=1C5D99"/></a>
+        <a target="blank"href="https://github.com/brycemara"><img alt="github" src="https://img.shields.io/badge/-Github-b1361e.svg?style=for-the-badge&logo=github&colorB=black" /></a>
+        <a target="blank" href="mailto:brycej98@gmail.com"><img alt="email" src="https://img.shields.io/badge/-Email-f2c236.svg?style=for-the-badge&colorB=0078D4" /></a>
       </div>
     </section>
   )
